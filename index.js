ab205224c94433b2cf597865413e74c8cc38ae2e
Susan Tan <susan.tan198@gmail.com>
2019-02-01 18:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
 
 /*
-it('defines `greeting`', function() {
-  expect(greeting).toEqual("Hello, everybody!")
-})
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
-})
 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
